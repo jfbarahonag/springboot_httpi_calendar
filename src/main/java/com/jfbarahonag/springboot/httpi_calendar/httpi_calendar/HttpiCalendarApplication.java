1e931bd7d04e448e8a4eac76a4c57cf4615822ca
jfbarahonag <jfbarahonag@unal.edu.co>
2024-07-23 18:05:15 -0500

    first commit

---
+++ src/main/java/com/jfbarahonag/springboot/httpi_calendar/httpi_calendar/HttpiCalendarApplication.java
@@ -0,0 +1,13 @@
+package com.jfbarahonag.springboot.httpi_calendar.httpi_calendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpiCalendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpiCalendarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jfbarahonag/springboot/httpi_calendar/httpi_calendar/HttpiCalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jfbarahonag.springboot.httpi_calendar.httpi_calendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpiCalendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
