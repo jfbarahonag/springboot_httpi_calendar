b13854fa4f57522b0c001f6008edec8b67634a3b
jfbarahonag <jfbarahonag@unal.edu.co>
2024-07-23 18:18:39 -0500

    adding config files

---
+++ src/main/java/com/jfbarahonag/springboot/httpi_calendar/httpi_calendar/AppConfig.java
@@ -0,0 +1,10 @@
+package com.jfbarahonag.springboot.httpi_calendar.httpi_calendar;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("config.properties")
+public class AppConfig {
+
+}
