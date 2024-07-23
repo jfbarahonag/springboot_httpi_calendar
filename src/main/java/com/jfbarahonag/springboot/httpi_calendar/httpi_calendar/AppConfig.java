047fefaf4e7d567b30be3172a5e500319a2ffc89
jfbarahonag <jfbarahonag@unal.edu.co>
2024-07-23 18:58:47 -0500

    registering the interceptor

--- src/main/java/com/jfbarahonag/springboot/httpi_calendar/httpi_calendar/AppConfig.java
+++ src/main/java/com/jfbarahonag/springboot/httpi_calendar/httpi_calendar/AppConfig.java
@@ -1,10 +1,24 @@
 package com.jfbarahonag.springboot.httpi_calendar.httpi_calendar;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@PropertySource("config.properties")
-public class AppConfig {
+@PropertySource("classpath:config.properties")
+public class AppConfig implements WebMvcConfigurer {
+
+  @Autowired
+  @Qualifier(("calendarInterceptor"))
+  HandlerInterceptor calendarInterceptor;
+
+  @Override
+  public void addInterceptors(InterceptorRegistry registry) {
+    registry.addInterceptor(calendarInterceptor);
+  }
 
 }
